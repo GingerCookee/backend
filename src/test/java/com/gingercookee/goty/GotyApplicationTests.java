f704a036f2f5111322d341f9f92328ccf9eff8a2
Ink-By <inkinkb@gmail.com>
2024-05-24 01:13:02 +0900

    init: set up new project

---
+++ src/test/java/com/gingercookee/goty/GotyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gingercookee.goty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
