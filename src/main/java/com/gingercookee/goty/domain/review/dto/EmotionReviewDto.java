44697d3a90ea6790f5010c366eb91690062253a9
Ink-By <inkinkb@gmail.com>
2024-11-02 04:48:48 -0700

    feat: 특정 감정에 따른 리뷰 DTO

---
+++ src/main/java/com/gingercookee/goty/domain/review/dto/EmotionReviewDto.java
@@ -0,0 +1,9 @@
+package com.gingercookee.goty.domain.Review.dto;
+import lombok.Getter;
+
+import java.util.Date;
+@Getter
+public class EmotionReviewDto {
+    private Date date;
+    private String content;
+}
