e91e4a2da4e2706cde2192f146b606640cfc7af2
Ink-By <inkinkb@gmail.com>
2024-11-02 03:39:24 -0700

    feat: ReviewController Rest functions

--- src/main/java/com/gingercookee/goty/domain/review/controller/ReviewController.java
+++ src/main/java/com/gingercookee/goty/domain/review/controller/ReviewController.java
@@ -1,5 +1,6 @@
-package com.gingercookee.goty.domain.review.controller;
+package com.gingercookee.goty.domain.Review.controller;
 
+import com.gingercookee.goty.domain.Review.dto.*;
 import com.gingercookee.goty.domain.review.dto.TopicReviewResponseDto;
 import com.gingercookee.goty.domain.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
@@ -7,9 +8,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.YearMonth;
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class ReviewController {
@@ -26,5 +28,35 @@ public class ReviewController {
         return ResponseEntity.ok(topicReviews);
     }
 
+    @GetMapping("{appId}/sentimentScore/{yearMonth}")
+    public List<SentimentScoreResponseDto> getSentimentScores(
+            @PathVariable Long appId,
+            @PathVariable("yearMonth") String yearMonth) {
+        YearMonth ym = YearMonth.parse(yearMonth);
+        return reviewService.getSentimentScore(appId, ym);
+    }
 
+    @GetMapping("{appId}/sentimentCount/{yearMonth}")
+    public SentimentCountDto getSentimentCount(
+            @PathVariable Long appId,
+            @PathVariable("yearMonth") String yearMonth) {
+        YearMonth ym = YearMonth.parse(yearMonth);
+        return reviewService.getSentimentCountByAppIdAndYearMonth(appId, ym);
+    }
+
+    @GetMapping("/{appId}/avgSentiment/week")
+    public List<WeeklySentimentProjection> getWeeklyAverageSentiment(@PathVariable Long appId) {
+        return reviewService.getWeeklyAverageSentiment(appId);
+    }
+
+    @GetMapping("/{appId}/avgSentiment/month")
+    public List<MonthlySentimentProjection> getMonthlyAverageSentiment(@PathVariable Long appId) {
+        return reviewService.getMonthlyAverageSentiment(appId);
+    }
+    @GetMapping("/{appId}/emotion/{yearMonth}")
+    public List<EmotionCountProjection> getEmotionCounts(@PathVariable Long appId,
+                                                         @PathVariable("yearMonth") String yearMonth) {
+        YearMonth ym = YearMonth.parse(yearMonth);
+        return reviewService.getEmotionCountsByAppIdAndMonth(appId, ym);
+    }
 }
